e7f03cf4b7ebd7ff76e4ea77d68c4db77c3c51ba
supplepentan <pentagramog@gmail.com>
2022-06-12 08:17:14 +0900

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,21 @@
 export const Header = () => {
     return (
         <div id="header" class="row bg-info">
-            <h1>ヘッダー</h1>
+            <nav class="navbar navbar-expand-lg navbar-light bg-info ">
+                <div class="container-fluid">
+                    <a class="navbar-brand text-light" href="/"><strong>Penta-Vite-Vanilla</strong></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
